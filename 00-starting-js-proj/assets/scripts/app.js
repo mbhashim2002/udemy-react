41fb865d62aeb097a998802e58c38f198bfd39c9
Phenoloki <phenoloki@gmail.com>
2024-11-28 23:55:57 +0800

    Added code for import/export syntax intro

---
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// import { apiKey, foo as bar } from "./util.js";
+// import defaultVal from "./util.js";
+
+import * as util from "./util.js";
+
+console.log(`The biggest API key: ${util.apiKey}\n${util.default}`);
+util.foo();
---
+++ 00-starting-js-proj/assets/scripts/util.js
@@ -0,0 +1,7 @@
+export let apiKey = "TheBigChungus";
+
+export default "The default string???!!1!";
+
+export const foo = () => {
+    console.log("This...is the dark souls of functions...");
+}
