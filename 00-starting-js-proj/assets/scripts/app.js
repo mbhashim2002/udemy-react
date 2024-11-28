fa5142ec0ddcc071322835711a3e4f2b2962f677
Phenoloki <phenoloki@gmail.com>
2024-11-29 00:02:25 +0800

    Added code for operators intro

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,7 +1,12 @@
-// import { apiKey, foo as bar } from "./util.js";
-// import defaultVal from "./util.js";
+// // import { apiKey, foo as bar } from "./util.js";
+// // import defaultVal from "./util.js";
 
-import * as util from "./util.js";
+// import * as util from "./util.js";
 
-console.log(`The biggest API key: ${util.apiKey}\n${util.default}`);
-util.foo();
+// console.log(`The biggest API key: ${util.apiKey}\n${util.default}`);
+// util.foo();
+
+console.log(`Adding numbers: ${10+5}`);
+console.log(`String concat: ${'hello'+'world'}`);
+console.log(`=== comparison: ${1 === 1}`);
+console.log(`Other bool operators: < > <= >=`);
