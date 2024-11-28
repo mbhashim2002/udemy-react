f8e759f09d3e9c909248825d26c5ab1f020d4d2b
Phenoloki <phenoloki@gmail.com>
2024-11-29 00:13:15 +0800

    Added code for functions intro

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -6,7 +6,17 @@
 // console.log(`The biggest API key: ${util.apiKey}\n${util.default}`);
 // util.foo();
 
-console.log(`Adding numbers: ${10+5}`);
-console.log(`String concat: ${'hello'+'world'}`);
-console.log(`=== comparison: ${1 === 1}`);
-console.log(`Other bool operators: < > <= >=`);
+// console.log(`Adding numbers: ${10+5}`);
+// console.log(`String concat: ${'hello'+'world'}`);
+// console.log(`=== comparison: ${1 === 1}`);
+// console.log(`Other bool operators: < > <= >=`);
+
+function greet(name = "Car", msg = "Meow") {
+    console.log(`Hello ${name}. ${msg}.`);
+    return `This is a message by the anti-${name} movement.`
+}
+
+greet();
+greet("Jane", "Big chungus");
+greet("Buh", "Guh");
+console.log(greet())
