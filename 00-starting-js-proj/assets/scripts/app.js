762b37be4a90cae80ee0b5b0e812e53fec7214fc
Phenoloki <phenoloki@gmail.com>
2024-12-05 15:42:14 +0800

    Added code for destructuring intro.

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -62,22 +62,50 @@
 // console.log(user1);
 // user1.greet();
 
-const hobbies = ["Sports", "Cooking", "Reading"];
-console.log(hobbies[0]);
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0]);
 
-hobbies.push("Working");
-console.log(hobbies);
+// hobbies.push("Working");
+// console.log(hobbies);
 
-const index = hobbies.findIndex(item => item === "Cooking");
+// const index = hobbies.findIndex(item => item === "Cooking");
 
-const index2 = hobbies.findIndex((item) => {
-    return item === "Cooking";
-})
+// const index2 = hobbies.findIndex((item) => {
+//     return item === "Cooking";
+// })
 
-console.log(index);
-console.log(index2);
+// console.log(index);
+// console.log(index2);
 
-//have to put object in () because otherwise the curly brackets will be interprested as function definers
-const editedHobbies = hobbies.map(item => ({text: item}));
+// //have to put object in () because otherwise the curly brackets will be interprested as function definers
+// const editedHobbies = hobbies.map(item => ({text: item}));
 
-console.log(editedHobbies);
+// console.log(editedHobbies);
+
+// const [firstName, lastName] = ["Big", "Chungus"];
+const userNameData = ["Big", "Chungus"];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+const [firstName, lastName] = userNameData;
+console.log(`${firstName} ${lastName}`);
+
+const { name: userName, age } = {
+    name: "Max",
+    age: 34,
+};
+
+console.log(`${userName} who is ${age} years old.`);
+
+function storeOrder(order) {
+    localStorage.setItem("id", order.id);
+    localStorage.setItem("currency", order.currency);
+}
+//can destructure function params above like so:
+function storeOrderDestructure({id, currency}){
+    localStorage.setItem("id", order.id);
+    localStorage.setItem("currency", order.currency);
+}
+
+storeOrder({id: 5, currency: 'USD', amount: 15.99});
