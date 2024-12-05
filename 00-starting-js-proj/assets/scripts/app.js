53a05359529a435e1e2108b6d066beebbe1e523b
Phenoloki <phenoloki@gmail.com>
2024-12-05 15:54:00 +0800

    More destructuring code.

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -82,30 +82,48 @@
 
 // console.log(editedHobbies);
 
-// const [firstName, lastName] = ["Big", "Chungus"];
-const userNameData = ["Big", "Chungus"];
+// // const [firstName, lastName] = ["Big", "Chungus"];
+// const userNameData = ["Big", "Chungus"];
 
-// const firstName = userNameData[0];
-// const lastName = userNameData[1];
+// // const firstName = userNameData[0];
+// // const lastName = userNameData[1];
 
-const [firstName, lastName] = userNameData;
-console.log(`${firstName} ${lastName}`);
+// const [firstName, lastName] = userNameData;
+// console.log(`${firstName} ${lastName}`);
 
-const { name: userName, age } = {
-    name: "Max",
-    age: 34,
+// const { name: userName, age } = {
+//     name: "Max",
+//     age: 34,
+// };
+
+// console.log(`${userName} who is ${age} years old.`);
+
+// function storeOrder(order) {
+//     localStorage.setItem("id", order.id);
+//     localStorage.setItem("currency", order.currency);
+// }
+// //can destructure function params above like so:
+// function storeOrderDestructure({id, currency}){
+//     localStorage.setItem("id", order.id);
+//     localStorage.setItem("currency", order.currency);
+// }
+
+// storeOrder({id: 5, currency: 'USD', amount: 15.99});
+
+const hobbies = ['Sports', 'Cooking'];
+const user = {
+    name: 'Max',
+    age: 34
 };
 
-console.log(`${userName} who is ${age} years old.`);
+const newHobbies = ['Reading'];
 
-function storeOrder(order) {
-    localStorage.setItem("id", order.id);
-    localStorage.setItem("currency", order.currency);
-}
-//can destructure function params above like so:
-function storeOrderDestructure({id, currency}){
-    localStorage.setItem("id", order.id);
-    localStorage.setItem("currency", order.currency);
+const mergedHobbies = [...hobbies, ...newHobbies];
+console.log(mergedHobbies);
+
+const extendedUser = {
+    isAdmin: true,
+    ...user
 }
 
-storeOrder({id: 5, currency: 'USD', amount: 15.99});
+console.log(extendedUser);
